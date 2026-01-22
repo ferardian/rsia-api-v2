599cab4413dd4cf283744e80fa2ce9a0566aa7b7
Ferry Ardiansyah <ferard.diansyah@gmail.com>
2026-01-22 09:46:32 +0700

    minor update riwayat obat

--- inspect_schema.php
+++
@@ -1,8 +0,0 @@
-<?php
-// inspect_schema.php
-$columns = \Illuminate\Support\Facades\DB::select("DESCRIBE riwayat_barang_medis");
-foreach ($columns as $col) {
-    if (in_array($col->Field, ['tanggal', 'jam'])) {
-        echo $col->Field . ": " . $col->Type . "\n";
-    }
-}
